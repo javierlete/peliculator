c81676ef97cc9c56a4fdaf5d77ee54930c846d21
javaee <javaee@javaee-VirtualBox>
2020-10-19 18:47:53 +0200

    Mensajes dentro del servidor para la consola

--- uf2216/src/com/ipartek/formacion/uf2216/sockets/Servidor.java
+++ uf2216/src/com/ipartek/formacion/uf2216/sockets/Servidor.java
@@ -10,14 +10,20 @@ public class Servidor {
 	private static final boolean AUTO_FLUSH = true;
 
 	public static void main(String[] args) {
+		System.out.println("Arrancando servidor");
+		
 		try (ServerSocket ss = new ServerSocket(1234);
 				Socket s = ss.accept();
 				PrintWriter out = new PrintWriter(s.getOutputStream(), AUTO_FLUSH);
 				//BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
 				 Scanner in = new Scanner(s.getInputStream())) {
+			System.out.println("Recibido un cliente");
+			
 			out.println("BIENVENIDO AL SERVIDOR MAYUSCULADOR");
 			
 			String texto = in.nextLine();
+			
+			System.out.println("Se ha recibido el texto " + texto);
 			
 			out.println(texto.toUpperCase());
 		} catch (IOException e) {
