4eaa4895d799bec5305d3b6096ed12b558265a28
Javier Lete <javier@eskura21.com>
2020-11-13 18:45:27 +0100

    (Formateado con VSCode)

--- uf2217/WebContent/JAVASCRIPT/js/javascript.js
+++ uf2217/WebContent/JAVASCRIPT/js/javascript.js
@@ -18,30 +18,30 @@
 
 function ejemplosBasicos() {
     'use strict';
-    
+
     var estaSeguro, numero;
-    
+
     estaSeguro = confirm('¿Estás seguro?');
     console.log(estaSeguro);
 
     numero = prompt('Introduce un número');
-    
+
     console.log(typeof numero);
     numero = parseInt(numero, 10);
-    
+
     if (isNaN(numero)) {//numero === NaN) {
         alert('Ya te vale. La próxima vez mete un número');
-        
+
         return;
     }
-    
+
     if (numero === 0) {
         alert('¡Mira que guay, es cero!');
     }
-    
+
     console.log(typeof numero);
     console.log(5 + numero);
-    
+
     console.log(+'5');
     //console.log(!!'5');
     //console.log(!!window.console);
@@ -49,48 +49,47 @@ function ejemplosBasicos() {
 
 function arrays() {
     'use strict';
-    
+
     var i, clave, arr = [5, 6, 7, 8];
-    
+
     console.log(arr[0]);
     console.log(arr[4]);
-    
+
     arr[1] = 10;
-    
+
     arr[4] = 20;
-    
+
     arr[5] = 'Yepa';
-    
+
     arr[8] = 'Despitorreeeeeee';
-    
-    //arr['Casa'] = 'Home';
-    arr.Casa = 'Home';
-    
+
+    arr['Casa'] = 'Home';
+
     arr.Coche = 'Car';
-    
+
     arr.alerta = alert;
-    
+
     arr.push(new Date());
-    
+
     console.log(arr);
-    
+
     console.log(arr.length);
-    
+
     var funcion = arr.alerta;
-    
+
     funcion('Hola desde funcion');
-    
+
     for (i = 0; i < arr.length; i++) {
         console.log(arr[i]);
     }
-    
+
     for (clave in arr) {
         console.log(clave, arr[clave]);
     }
-    
-//    for (var dato of arr) {
-//        console.log(dato);
-//    }
+
+    //    for (var dato of arr) {
+    //        console.log(dato);
+    //    }
 }
 
 arrays();
