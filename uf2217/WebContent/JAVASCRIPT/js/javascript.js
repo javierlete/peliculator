d43e3ed71d5a2057c40e31980ae88bb817015645
Javier Lete <javier@eskura21.com>
2020-11-17 15:44:22 +0100

    Ejemplo de switch sin break

--- uf2217/WebContent/JAVASCRIPT/js/javascript.js
+++ uf2217/WebContent/JAVASCRIPT/js/javascript.js
@@ -196,3 +196,18 @@
 
     //ejemplosBasicos();
 })();
+
+function diasMes(mes) {
+    var dias;
+
+    switch (mes) {
+        case 2: dias = 28; break;
+        case 4:
+        case 6:
+        case 9:
+        case 11: dias = 30; break;
+        default: dias = 31;
+    }
+
+    return dias;
+}
