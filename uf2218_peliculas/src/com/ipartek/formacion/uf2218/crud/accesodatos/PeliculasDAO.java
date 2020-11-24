44a927961388ba466fc4c28db9b61e01f3b05f9d
Javier Lete <javier@eskura21.com>
2020-11-24 18:29:09 +0100

    Capas de accesodatos y entidades (del uf2216)

---
+++ uf2218_peliculas/src/com/ipartek/formacion/uf2218/crud/accesodatos/PeliculasDAO.java
@@ -0,0 +1,45 @@
+package com.ipartek.formacion.uf2218.crud.accesodatos;
+
+import java.time.LocalDate;
+import java.util.TreeMap;
+
+import com.ipartek.formacion.uf2218.crud.entidades.Pelicula;
+
+public class PeliculasDAO {
+	private PeliculasDAO() {}
+	
+	private static TreeMap<Long, Pelicula> peliculas = new TreeMap<>();
+
+	static {
+		peliculas.put(1L, new Pelicula(1L, "Indiana Jones", "Accion", LocalDate.of(1980, 6, 5)));
+		peliculas.put(2L, new Pelicula(2L, "Superman", "Accion", LocalDate.of(1983, 1, 2)));
+	}
+	
+	public static Iterable<Pelicula> obtenerTodas() {
+		return peliculas.values();
+	}
+
+	public static Pelicula obtenerPorId(Long id) {
+		return peliculas.get(id);
+	}
+	
+	public static void insertar(Pelicula pelicula) {
+		Long id = peliculas.lastKey() + 1;
+		pelicula.setId(id);
+		peliculas.put(pelicula.getId(), pelicula);
+	}
+
+	public static void modificar(Pelicula pelicula) {
+		if(!peliculas.containsKey(pelicula.getId())) {
+			throw new AccesoDatosException("No existe ese id");
+		}
+		peliculas.put(pelicula.getId(), pelicula);
+	}
+
+	public static void borrar(Long id) {
+		if(!peliculas.containsKey(id)) {
+			throw new AccesoDatosException("No existe ese id");
+		}
+		peliculas.remove(id);
+	}
+}
