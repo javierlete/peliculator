803c5a687f9d2eeb93f6814f986557a2030abc64
Javier Lete <javier@eskura21.com>
2020-12-03 18:50:28 +0100

    Proyecto de ejemplo de JDBC

---
+++ uf2213_jdbc/src/test/java/com/ipartek/formacion/uf2213_jdbc/AppTest.java
@@ -0,0 +1,20 @@
+package com.ipartek.formacion.uf2213_jdbc;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
